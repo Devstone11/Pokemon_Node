a3bd9add9a4654d5314bf6dae121e9eeb88dedc0
Devin Stone <devstone11@gmail.com>
2016-07-21 15:51:56 -0600

    Complete gym story 2

--- routes/index.js
+++ routes/index.js
@@ -8,9 +8,25 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/gym', function(req, res, next) {
-  Pokemon.all('pokemon').then(function(pokemon) {
-    res.render('gym/index', {pokemon: pokemon.rows});
-  })
+  if (req.cookies.p1 && req.cookies.p2) {
+    console.log(req.cookies.p1);
+    Pokemon.find('pokemon', req.cookies.p1).then(function(pokemon1) {
+      Pokemon.find('pokemon', req.cookies.p2).then(function(pokemon2) {
+        res.render('gym/index', {pokemon1: pokemon1.rows[0], pokemon2: pokemon2.rows[0]})
+      })
+    })
+  } else {
+    Pokemon.all('pokemon').then(function(pokemon) {
+      res.render('gym/edit', {pokemon: pokemon.rows});
+    })
+  }
+})
+
+router.post('/gym', function(req, res, next) {
+  console.log(req.body);
+  res.cookie('p1', req.body.pokemon1);
+  res.cookie('p2', req.body.pokemon2);
+  res.redirect('/gym');
 })
 
 module.exports = router;
