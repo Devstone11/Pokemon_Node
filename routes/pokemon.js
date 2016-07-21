454a641b76917ccfc18b8fd2ceb30a2594cb0d14
Devin Stone <devstone11@gmail.com>
2016-07-21 14:42:54 -0600

    refactored routes

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var knex = require('../db/knex');
+var Pokemon = require('../lib/queries');
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-  knex.raw(`SELECT * from pokemon`).then(function(pokemon) {
+  Pokemon.all('pokemon').then(function(pokemon) {
     var gymFull = (req.cookies.p1 && req.cookies.p2) ? true : false;
     res.render('pokemon/index', {pokemon: pokemon.rows, gymFull: gymFull});
   })
@@ -15,51 +15,49 @@ router.get('/new', function(req, res, next) {
 });
 
 router.post('/', function(req, res, next) {
-  console.log(req.body);
-  knex.raw(`INSERT into pokemon values (DEFAULT, '${req.body.name}', ${req.body.trainer_id}, ${req.body.cp}, 'f')`).then(function() {
+  Pokemon.create(req.body).then(function() {
     res.redirect('/');
   });
 })
 
 router.get('/:id', function(req, res, next) {
-  knex.raw(`SELECT * from pokemon WHERE id=${req.params.id}`).then(function(pokemon) {
-    knex.raw(`SELECT * from trainers WHERE id=${pokemon.rows[0].trainer_id}`).then(function(trainer) {
+  Pokemon.find('pokemon', req.params.id).then(function(pokemon) {
+    Pokemon.find('trainers', pokemon.rows[0].trainer_id).then(function(trainer) {
       res.render('pokemon/show', {pokemon: pokemon.rows[0], trainer: trainer.rows[0]})
     })
   })
 })
 
 router.get('/:id/edit', function(req, res, next) {
-  knex.raw(`SELECT * from pokemon WHERE id=${req.params.id}`).then(function(pokemon) {
-    knex.raw(`SELECT * from trainers`).then(function(trainers) {
+  Pokemon.find('pokemon', req.params.id).then(function(pokemon) {
+    Pokemon.all('trainers').then(function(trainers) {
       res.render('pokemon/edit', {pokemon: pokemon.rows[0], trainers: trainers.rows})
     })
   })
 })
 
 router.post('/:id', function(req, res, next) {
-  knex.raw(`UPDATE pokemon SET name='${req.body.name}', trainer_id=${req.body.trainer_id}, cp=${req.body.cp}
-  WHERE id=${req.params.id}`).then(function() {
+  Pokemon.updateOne(req.body, req.params.id).then(function() {
     res.redirect('/pokemon/'+req.params.id);
   })
 })
 
 router.get('/:id/assign-gym', function(req, res, next) {
-  knex.raw(`UPDATE pokemon SET in_gym='t' WHERE id=${req.params.id}`).then(function() {
+  Pokemon.setGym('t', req.params.id).then(function() {
     (req.cookies.p1) ? res.cookie('p2', req.params.id) : res.cookie('p1', req.params.id);
     res.redirect('/');
   })
 })
 
 router.get('/:id/remove-gym', function(req, res, next) {
-  knex.raw(`UPDATE pokemon SET in_gym='f' WHERE id=${req.params.id}`).then(function() {
+  Pokemon.setGym('f', req.params.id).then(function() {
     (req.cookies.p1 === req.params.id) ? res.clearCookie('p1') : res.clearCookie('p2');
     res.redirect('/');
   })
 })
 
 router.get('/:id/delete', function(req, res, next) {
-  knex.raw(`DELETE from pokemon WHERE id=${req.params.id}`).then(function() {
+  Pokemon.delete(req.params.id).then(function() {
     res.redirect('/');
   })
 })
