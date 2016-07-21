66d426190d39c51dde130264916a905596b3d674
Devin Stone <devstone11@gmail.com>
2016-07-21 15:29:52 -0600

    complete gym story 1

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var Pokemon = require('../lib/queries');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.redirect('/pokemon');
 });
+
+router.get('/gym', function(req, res, next) {
+  Pokemon.all('pokemon').then(function(pokemon) {
+    res.render('gym/index', {pokemon: pokemon.rows});
+  })
+})
 
 module.exports = router;
