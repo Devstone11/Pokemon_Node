86037fea7ecb58522fe33ba4c2a7c787f91ba391
Devin Stone <devstone11@gmail.com>
2016-07-21 16:36:03 -0600

    complete gym stories 3 and 4

--- routes/index.js
+++ routes/index.js
@@ -9,22 +9,32 @@ router.get('/', function(req, res, next) {
 
 router.get('/gym', function(req, res, next) {
   if (req.cookies.p1 && req.cookies.p2) {
-    console.log(req.cookies.p1);
     Pokemon.find('pokemon', req.cookies.p1).then(function(pokemon1) {
       Pokemon.find('pokemon', req.cookies.p2).then(function(pokemon2) {
         res.render('gym/index', {pokemon1: pokemon1.rows[0], pokemon2: pokemon2.rows[0]})
       })
     })
   } else {
-    Pokemon.all('pokemon').then(function(pokemon) {
-      res.render('gym/edit', {pokemon: pokemon.rows});
-    })
+    res.redirect('/gym/edit');
   }
+})
+
+router.get('/gym/edit', function(req, res, next) {
+  Pokemon.all('pokemon').then(function(pokemon) {
+    if (req.cookies.p1 || req.cookies.p2) {
+      var id = req.cookies.p1 || req.cookies.p2;
+      Pokemon.find('pokemon', id).then(function(pokemon1) {
+        res.render('gym/edit', {pokemon: pokemon.rows, pokemon1: pokemon1.rows[0]});
+      })
+    } else {
+      res.render('gym/edit', {pokemon: pokemon.rows, pokemon1: false});
+    }
+  })
 })
 
 router.post('/gym', function(req, res, next) {
   console.log(req.body);
-  res.cookie('p1', req.body.pokemon1);
+  res.cookie('p1', req.body.pokemon1 || req.cookies.p1 || req.cookies.p2);
   res.cookie('p2', req.body.pokemon2);
   res.redirect('/gym');
 })
